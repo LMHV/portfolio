a7d421740bd43e09fc466b3c5ae8b1ec5d5d94cc
Lucho <luis.herrero.facultad@gmail.com>
2024-01-08 15:57:25 -0300

    commit + 8/1

--- src/utils/Tags.ts
+++ src/utils/Tags.ts
@@ -2,6 +2,14 @@ import React from '../components/icons/React.astro'
 import Python from '../components/icons/Python.astro'
 import MongoDB from '../components/icons/MongoDB.astro'
 import Qt from '../components/icons/Qt.astro'
+import Bootstrap from '@/components/icons/Bootstrap.astro'
+import Javascript from '@/components/icons/Javascript.astro'
+import Html from '@/components/icons/Html.astro'
+import Css from '@/components/icons/Css.astro'
+import Nodejs from '@/components/icons/Nodejs.astro'
+import Expressjs from '@/components/icons/Expressjs.astro'
+import Flutter from '@/components/icons/Flutter.astro'
+import Dart from '@/components/icons/Dart.astro'
 
 export const TAGS = {
   REACT: {
@@ -12,16 +20,57 @@ export const TAGS = {
   PYTHON: {
     name: 'Python',
     icon: Python,
-    class: 'text-sky-600 border-yellow-600',
+    class: 'text-sky-600',
   },
   MONGODB: {
     name: 'MongoDB',
     icon: MongoDB,
-    class: 'text-green-600 border-green-600',
+    class: 'text-green-600',
   },
   QT: {
     name: 'Qt',
     icon: Qt,
     class: 'text-white',
   },
+  BOOTSTRAP: {
+    name: 'Bootstrap',
+    icon: Bootstrap,
+    class: 'text-[#7b1be9]',
+  },
+  JAVASCRIPT: {
+    name: 'Javascript',
+    icon: Javascript,
+    class: 'text-[#e2cf51]',
+  },
+  HTML: {
+    name: 'HTML',
+    icon: Html,
+    class: 'text-[#d6512c]'
+  },
+  CSS: {
+    name: 'CSS',
+    icon: Css,
+    class: 'text-[#1571af]',
+  },
+  NODEJS: {
+    name: 'NodeJs',
+    icon: Nodejs,
+    class: 'text-[#559846]'
+  },
+  EXPRESSJS: {
+    name: 'ExpressJs',
+    icon: Expressjs,
+    class: 'text-neutral-300'
+  },
+  FLUTTER: {
+    name: 'Flutter',
+    icon: Flutter,
+    class: 'text-neutral-300'
+  },
+  DART: {
+    name: 'Dart',
+    icon: Dart,
+    class: 'text-neutral-300'
+  },
+
 }
